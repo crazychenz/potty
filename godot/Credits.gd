d4337bb091f1e77cbf1bac9cd33709377799ecef
CrazyChenz <crazychenz@gmail.com>
2020-03-13 18:14:06 -0400

    Added WASD as keys for player. Added credit roll screen

---
+++ godot/Credits.gd
@@ -0,0 +1,16 @@
+extends Node
+
+onready var tween : Tween = find_node("Tween")
+onready var container : ScrollContainer = find_node("ScrollContainer")
+onready var MainMenuButton : Button = find_node("MainMenuButton")
+onready var CreditsLabel : Label = find_node("CreditsLabel")
+
+func _ready() -> void:
+    var creditsSize : Vector2 = CreditsLabel.get_combined_minimum_size()
+    MainMenuButton.connect("pressed", self, "_on_MainMenuButton_pressed")
+    tween.interpolate_property(container, "scroll_vertical",
+        0, creditsSize.y, 10, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+    tween.start()
+
+func _on_MainMenuButton_pressed() -> void:
+    get_tree().change_scene("res://Title.tscn")
--- godot/TitleUI.gd
+++ godot/TitleUI.gd
@@ -2,13 +2,18 @@ extends Control
 
 onready var NewGameButton = find_node("NewGameButton")
 onready var QuitButton = find_node("QuitButton")
+onready var CreditsButton = find_node("CreditsButton")
 
 func _ready() -> void:
     NewGameButton.connect("pressed", self, "_on_NewGameButton_pressed")
     QuitButton.connect("pressed", self, "_on_QuitButton_pressed")
+    CreditsButton.connect("pressed", self, "_on_CreditsButton_pressed")
 
 func _on_NewGameButton_pressed() -> void:
     get_tree().change_scene("res://Main.tscn")
 
 func _on_QuitButton_pressed() -> void:
     get_tree().quit()
+
+func _on_CreditsButton_pressed() -> void:
+    get_tree().change_scene("res://Credits.tscn")
