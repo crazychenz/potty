ae1a2d7900bc012e2b2672555ebdd98685a33341
CrazyChenz <crazychenz@gmail.com>
2020-05-04 11:27:56 -0400

    Added operators for Vector2.hpp

--- godot/PottyModel/include/model/Vector2.hpp
+++ godot/PottyModel/include/model/Vector2.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
-class Vector2 {
+#include <iostream>
+
+class Vector2i {
 
 private:
     int _x;
     int _y;
 
 public:
-    Vector2() /*: x(_x), y(_y)*/ {}
-    Vector2(int x_param, int y_param) : _x(x_param), _y(y_param)/*, x(_x), y(_y)*/ {}
-    Vector2(const Vector2 &vec) : _x(vec.getX()), _y(vec.getY())/*, x(_x), y(_y)*/ {}
-    ~Vector2() {}
+    Vector2i() /*: x(_x), y(_y)*/ {}
+    Vector2i(int x_param, int y_param) : _x(x_param), _y(y_param)/*, x(_x), y(_y)*/ {}
+    Vector2i(const Vector2i &vec) : _x(vec.getX()), _y(vec.getY())/*, x(_x), y(_y)*/ {}
+    ~Vector2i() {}
 
     std::wstring toStr()
     {
@@ -23,6 +25,113 @@ public:
     //const int &x;
     //const int &y;
 
+    friend std::ostream& operator<<(std::ostream& os, const Vector2i& dt)
+    {
+        os << "(" << dt.getX() << ", " << dt.getY() << ")";
+        return os;
+    }
 
+    Vector2i& operator+=(const Vector2i& rhs)
+    {
+        _x += rhs.getX();
+        _y += rhs.getY();
+        return *this;
+    }
+
+    Vector2i& operator-=(const Vector2i &rhs)
+    {
+        _x -= rhs.getX();
+        _y -= rhs.getY();
+        return *this;
+    }
+
+    friend Vector2i operator+(const Vector2i &lhs, const Vector2i &rhs)
+    {
+        return Vector2i(lhs.getX() + rhs.getX(), lhs.getY() + rhs.getY());
+    }
+
+    friend Vector2i operator-(const Vector2i &lhs, const Vector2i &rhs)
+    {
+        return Vector2i(lhs.getX() - rhs.getX(), lhs.getY() - rhs.getY());
+    }
+
+    friend bool operator==(const Vector2i &lhs, const Vector2i &rhs)
+    {
+        return lhs.getX() == rhs.getX() && lhs.getY() == rhs.getY();
+    }
+
+    friend bool operator!=(const Vector2i &lhs, const Vector2i &rhs)
+    {
+        return lhs.getX() != rhs.getX() || lhs.getY() != rhs.getY();
+    }
 
 };
+
+typedef Vector2i Vector2;
+
+class Vector2f {
+
+private:
+    float _x;
+    float _y;
+
+public:
+    Vector2f() /*: x(_x), y(_y)*/ {}
+    Vector2f(int x_param, int y_param) : _x(x_param), _y(y_param)/*, x(_x), y(_y)*/ {}
+    Vector2f(const Vector2f &vec) : _x(vec.getX()), _y(vec.getY())/*, x(_x), y(_y)*/ {}
+    ~Vector2f() {}
+
+    std::wstring toStr()
+    {
+        return L"(" + std::to_wstring(getX()) + L", " + std::to_wstring(getY()) + L")";
+    }
+
+    int getX() const { return _x; }
+    int getY() const { return _y; }
+
+    //const int &x;
+    //const int &y;
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector2f& dt)
+    {
+        os << "(" << dt.getX() << ", " << dt.getY() << ")";
+        return os;
+    }
+
+    Vector2f& operator+=(const Vector2f& rhs)
+    {
+        _x += rhs.getX();
+        _y += rhs.getY();
+        return *this;
+    }
+
+    Vector2f& operator-=(const Vector2f &rhs)
+    {
+        _x -= rhs.getX();
+        _y -= rhs.getY();
+        return *this;
+    }
+
+    friend Vector2f operator+(const Vector2f &lhs, const Vector2f &rhs)
+    {
+        return Vector2f(lhs.getX() + rhs.getX(), lhs.getY() + rhs.getY());
+    }
+
+    friend Vector2f operator-(const Vector2f &lhs, const Vector2f &rhs)
+    {
+        return Vector2f(lhs.getX() - rhs.getX(), lhs.getY() - rhs.getY());
+    }
+
+    friend bool operator==(const Vector2f &lhs, const Vector2f &rhs)
+    {
+        return lhs.getX() == rhs.getX() && lhs.getY() == rhs.getY();
+    }
+
+    friend bool operator!=(const Vector2f &lhs, const Vector2f &rhs)
+    {
+        return lhs.getX() != rhs.getX() || lhs.getY() != rhs.getY();
+    }
+
+};
+
+
